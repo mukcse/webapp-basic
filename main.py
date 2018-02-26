ff003981f8b010b668f17fb7e7dc25f329b14e74
mukcse <mukcse@gmail.com>
2018-02-26 13:01:20 +0600

    step 2: google search

--- main.py
+++ main.py
@@ -14,11 +14,17 @@
 
 import webapp2
 
+form = """
+<form action="http://www.google.com/search">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
 
 
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
